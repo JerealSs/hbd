11b4611c0697e20f2f93a9c45ce6278e353e3672
JerealSs <coc.azriel@gmail.com>
2026-07-30 02:19:10 +0700

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,7 @@
+document.getElementById('prev-page').addEventListener('click', () => {
+  window.location.href = 'our-story.html';
+});
+
+document.getElementById('next-page').addEventListener('click', () => {
+  window.location.href = 'letter.html';
+});
